7ee18c2b23dfa48e8df1d9628fc20fec7c90f48b
dpessanha <levita.dw@gmail.com>
2021-03-20 07:22:27 -0300

    Header & Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () =>{
   return (
     <>
+    <Header />
+    <main className='py-3'>
+      <Container>
       <h1>Bem vindo a Trattoria Di Nando!</h1>
+      </Container>
+    </main>
+    <Footer />
     </>
   );
 }
