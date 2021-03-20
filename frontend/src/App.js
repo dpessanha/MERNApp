de99437ca31a391e659f6b78b0eb66c9d233cbc4
dpessanha <levita.dw@gmail.com>
2021-03-20 09:30:31 -0300

    HomeScreen component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
-const App = () =>{
+const App = () => {
   return (
     <>
-    <Header />
-    <main className='py-3'>
-      <Container>
-      <h1>Bem vindo a Trattoria Di Nando!</h1>
-      </Container>
-    </main>
-    <Footer />
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
