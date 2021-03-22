ba955fb621b57bd6812324e16ceba69125351606
daopessanha <daopessanha@gmail.com>
2021-03-22 14:43:31 -0300

    nodemon && concurrently setup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Nav, Navbar, Container, Image } from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 
 const Header = () => {
   return (
